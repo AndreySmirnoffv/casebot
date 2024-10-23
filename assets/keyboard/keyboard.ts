61a084dfdfc0987df812d2a70501ead96a017bd6
Andrey Smirnoff <149796739+AndreySmirnoffv@users.noreply.github.com>
2024-10-23 20:16:07 +0200

    Delete assets directory

--- assets/keyboard/keyboard.ts
+++
@@ -1,8 +0,0 @@
-
-export const webAppKeyboard = {
-    reply_markup: {
-        inline_keyboard: [
-            [{text: "web app", webApp: {url: "https://crypto-drop-rouge.vercel.app/"}, callback_data: "hello"}]
-        ]
-    }
-}
