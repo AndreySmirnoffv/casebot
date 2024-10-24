d7f02e28ba43340b09378a60594583b0d733ba1f
Andrey Smirnoff <149796739+AndreySmirnoffv@users.noreply.github.com>
2024-10-24 17:23:17 +0200

    Update payment.service.ts

--- server/src/services/payment.service.ts
+++ server/src/services/payment.service.ts
@@ -1,6 +1,6 @@
 import Stripe from "stripe";
 
-const stripe = new Stripe('sk_test_51OkRe7KsvhRur5TZI1OezYZ1cfRffOeewWByGUPRz5sAGvjQBJY07B1iueIplIXK3VzJI27u5VDGMkqS1U7X7jia002CfMo36a');
+const stripe = new Stripe('');
 
 export async function createStripePayment(amount: number, currency: string){
     return await stripe.paymentIntents.create({
