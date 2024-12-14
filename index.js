0292a17dc89a0cb38fce145f6d1f3571a408e671
Andrey Smirnoff <149796739+AndreySmirnoffv@users.noreply.github.com>
2024-12-14 02:27:23 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 import TelegramBot from "node-telegram-bot-api";
 import dotenv from 'dotenv';
-import { addUser, checkUserExists } from './assets/scripts/insertUser.js';
-import { webAppKeyboard } from './assets/keyboard/keyboard.js';
+import { addUser, checkAdmin, checkUserExists } from './assets/scripts/insertUser.js';
+import { adminKeyboard, webAppKeyboard } from './assets/keyboard/keyboard.js';
 import axios from "axios";
+import { addUserPercantage } from "./assets/scripts/adminFunctions.js";
 
 dotenv.config({ path: './assets/modules/.env' });
 
 const bot = new TelegramBot(process.env.TOKEN, { polling: true });
 
 bot.on('message', async (msg) => {
-    if (msg.text.includes("/start")) {
+    if (msg.text?.includes("/start")) {
         const ref = `${process.env.BOT_LINK}?start=${msg.chat?.id}`;
         console.info(`Referral link: ${ref}`);
-        console.log(msg)
+        console.log(msg);
+
         const userId = msg.from?.id;
         const chatId = msg.chat?.id;
         const firstName = msg.from?.first_name;
@@ -26,18 +28,36 @@ bot.on('message', async (msg) => {
             }
 
             if (!exists) {
-                await addUser(userId, chatId, firstName, lastName, username, ref, origin, 0, 0, 0, (error) => {
-                    if (error) {
-                        return console.error('Error adding user.');
-                    }
-                    console.info('User added to the database');
-                });
+                try {
+                    await addUser(
+                        userId,             // id
+                        username,           // username
+                        firstName,          // firstName
+                        lastName,           // lastName
+                        ref,                // ref
+                        origin, 
+                        chatId,            // origin
+                        0,                  // balance (начальный баланс)
+                        0,                  // cryptoBalance (начальный крипто-баланс)
+                        false               // admin (по умолчанию false)
+                    );
+                } catch (error) {
+                    console.error("Failed to add user:", error.message);
+                }
             }
 
-            await bot.sendMessage(msg.chat?.id, `Привет ${msg.from?.username}`, webAppKeyboard);
+            checkAdmin(userId, bot, msg);
+
+
             console.info('User already exists in the database');
         });
     }
 });
+
+bot.on('callback_query', async (msg) => {
+    if(msg.data === "add_percantage_user"){
+        await addUserPercantage(bot, msg.message.chat.id)
+    }
+})
 
 bot.on('polling_error', console.log);
